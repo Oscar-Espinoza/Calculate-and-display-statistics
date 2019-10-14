304bc4d9a2799629b0b036fae84231ebc5fa5ea0
unknown <oscardavid2094@gmail.com>
2019-10-13 23:52:26 -0300

    Glance table lista

--- main.js
+++ main.js
@@ -37,48 +37,89 @@ function createTable(data) {
     document.getElementById("senate-data").innerHTML = table;
 }
 
-function createHagTable(data) {
+function getGlanceData(members){
 
-    dataMembers = data.results[0].members;
-    getTotalOf = (party) => {
-
+    let glanceData = {
+      'Republicans':{
+        'party': 'Republicans',
+        'total': 0,
+        'votesWithParty': 0},
+      'Democrats':{
+        'party': 'Democrats',
+        'total': 0,
+        'votesWithParty': 0},
+      'Independents':{
+        'party':'Independents',
+        'total': 0,
+        'votesWithParty': 0},
+      'Total':{
+        'party': 'Total',
+        'total': 0,
+        'votesWithParty':0,
+      }
     }
 
-    let HagData = {
-            'Republicans': {
-                'Total':
-            }
-        },
-        'Democrats': {
+    const {Republicans, Democrats, Independents, Total} = glanceData
 
-        },
-        'Independents': {
+    members.forEach(member => {
+      switch (member.party) {
+        case 'D':
+          Democrats.total += 1;
+          Democrats.votesWithParty += member.votes_with_party_pct;
+        break;
 
-        }
-};
+        case 'R':
+          Republicans.total += 1;
+          Republicans.votesWithParty += member.votes_with_party_pct;
+        break;
 
-let tableHead = "<tr>";
-let table;
-for (let propertyName in memberProperties) {
-    tableHead += "<th>" + propertyName + "</th>";
+        default:
+          Independents.total += 1;
+          Independents.votesWithParty += member.votes_with_party_pct;
+        break;
+      }
+    })
+
+    for (let party in glanceData){
+      if (glanceData[party].total != 0) {
+        glanceData[party].votesWithParty /= glanceData[party].total  
+      }      
+    }
+    
+    Total.total = Independents.total + Democrats.total + Republicans.total
+    Total.votesWithParty = Independents.votesWithParty + Democrats.votesWithParty + Republicans.votesWithParty
+
+    return glanceData
 }
 
-dataMembers.forEach(member => {
+function createGlanceTable(data) {
 
-    if (member.middle_name == null) {
-        member.middle_name = "";
-    }
+  const dataMembers = data.results[0].members;
+  const tableData = getGlanceData(dataMembers);
+  let tableHead = "<tr>";
+  let tableProperties = {
+        'Party': 'party',
+        'Number of Representants': 'total',
+        '% Votes with party': 'votesWithParty'
+    };
 
-    member.votes_with_party_pct = member.votes_with_party_pct.toString() + " %";
-    member.first_name = "<a href='" + member.url + "' target='_blank'>" + member.first_name + "</a>";
+  for (let propertyName in tableProperties) {
+      tableHead += "<th>" + propertyName + "</th>";
+  }
 
-    table += "<tr>";
-    for (let property in memberProperties) {
-        table = table + "<td>" + member[memberProperties[property]] + "</td>";
-    }
-    table += "</tr>";
-});
+  tableHead+="</tr>";
+
+let table = "";
+
+for (party in tableData){  
+  table+="<tr>";
+  for(let property in tableProperties){
+    property = tableProperties[property];
+    table += `<td>${tableData[party][property]}`;
+  }
+  table+="</tr>";
+}
 
 table = tableHead + table;
-document.getElementById("HagTable").innerHTML = table;
+document.getElementById("GlanceTable").innerHTML = table;
 }
