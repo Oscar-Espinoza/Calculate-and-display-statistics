6fb5291bf5e52530e1d440a11b8fc56abda35b37
unknown <oscardavid2094@gmail.com>
2019-10-14 22:03:07 -0300

    Calculate Statistics terminado

--- main.js
+++ main.js
@@ -39,70 +39,142 @@ function createTable(data) {
 
 function getGlanceData(members){
 
-    let glanceData = {
-      'Republicans':{
-        'party': 'Republicans',
-        'total': 0,
-        'votesWithParty': 0},
-      'Democrats':{
-        'party': 'Democrats',
-        'total': 0,
-        'votesWithParty': 0},
-      'Independents':{
-        'party':'Independents',
-        'total': 0,
-        'votesWithParty': 0},
-      'Total':{
-        'party': 'Total',
-        'total': 0,
-        'votesWithParty':0,
-      }
-    }
+  let glanceData = [
+          {'party': 'Republicans',
+          'total': 0,
+          'votesWithParty': 0},
+          {'party': 'Democrats',
+          'total': 0,
+          'votesWithParty': 0},
+          {'party':'Independents',
+          'total': 0,
+          'votesWithParty': 0},
+          {'party': 'Total',
+          'total': 0,
+          'votesWithParty':0},
+      ]
+      const [Republicans, Democrats, Independents, Total] = glanceData
+      
+      members.forEach(member => {      
+        switch (member.party) {
+          case 'D':
+            Democrats.total += 1;
+            Democrats.votesWithParty += member.votes_with_party_pct;
+          break;
 
-    const {Republicans, Democrats, Independents, Total} = glanceData
+          case 'R':
+            Republicans.total += 1;
+            Republicans.votesWithParty += member.votes_with_party_pct;
+          break;
 
-    members.forEach(member => {
-      switch (member.party) {
-        case 'D':
-          Democrats.total += 1;
-          Democrats.votesWithParty += member.votes_with_party_pct;
-        break;
+          default:
+            Independents.total += 1;
+            Independents.votesWithParty += member.votes_with_party_pct;
+          break;
+        }
+      })
 
-        case 'R':
-          Republicans.total += 1;
-          Republicans.votesWithParty += member.votes_with_party_pct;
-        break;
-
-        default:
-          Independents.total += 1;
-          Independents.votesWithParty += member.votes_with_party_pct;
-        break;
-      }
-    })
-
-    for (let party in glanceData){
-      if (glanceData[party].total != 0) {
-        glanceData[party].votesWithParty /= glanceData[party].total  
-      }      
-    }
-    
-    Total.total = Independents.total + Democrats.total + Republicans.total
-    Total.votesWithParty = Independents.votesWithParty + Democrats.votesWithParty + Republicans.votesWithParty
-
-    return glanceData
+      glanceData.forEach(party => {
+        if (party.total != 0) {
+          party.votesWithParty /= party.total
+        }
+      });
+      
+      Total.total = Independents.total + Democrats.total + Republicans.total
+      Total.votesWithParty = Independents.votesWithParty + Democrats.votesWithParty + Republicans.votesWithParty
+      return glanceData
 }
 
-function createGlanceTable(data) {
+function getAttendanceData(tableID, members, propertyFilter){
+
+  let AttendanceData = [];
+  if (tableID == "Least") {
+    members.sort((a, b) => {
+      if (a.missed_votes < b.missed_votes) {return 1} 
+      else if (a.missed_votes > b.missed_votes) {return -1} 
+      else {return 0}      
+    })
+  } else if (tableID == "Most") {
+    members.sort((a, b) => {
+      if (a.missed_votes > b.missed_votes) {return 1} 
+      else if (a.missed_votes < b.missed_votes) {return -1} 
+      else {return 0}
+    })
+  }
+
+  for (let i = 0; i < Math.round(members.length/10); i++) {
+    AttendanceData.push(members[i])
+  }
+  return AttendanceData
+
+}
+
+function GetLoyaltyData(tableID, members){
+  let AttendanceData = [];
+  if (tableID == "Least") {
+    members.sort((a, b) => {
+      if ((a.total_votes) < (b.total_votes)) {return 1} 
+      else if ((a.total_votes) > (b.total_votes)) {return -1} 
+      else {return 0}      
+    })
+  } else if (tableID == "Most") {
+    members.sort((a, b) => {
+      if ((a.total_votes) > (b.total_votes)) {return 1} 
+      else if ((a.total_votes) < (b.total_votes)) {return -1} 
+      else {return 0}
+    })
+  }
+
+  for (let i = 0; i < Math.round(members.length/10); i++) {
+    AttendanceData.push(members[i])
+  }
+  
+  return AttendanceData
+
+}
+
+
+function getTableData(tableID, members){
+
+let tableData;
+
+  switch (tableID) {
+    case 'GlanceTable':
+      tableData = getGlanceData(members)
+    break;
+
+    case 'Least Engagged':
+    tableData = getAttendanceData(tableID, members)
+    break;
+
+    case 'Most Engagged':
+    tableData = getAttendanceData(tableID, members)
+    break;
+
+    case 'Most Loyal':
+      tableData = GetLoyaltyData(tableID, members)
+    break;
+    case 'Least Loyal':
+      tableData = GetLoyaltyData(tableID, members)
+    break;
+  }
+
+  return tableData
+
+}
+
+function createCalculatedTable(tableID, data, tableProperties) {
 
   const dataMembers = data.results[0].members;
-  const tableData = getGlanceData(dataMembers);
-  let tableHead = "<tr>";
-  let tableProperties = {
-        'Party': 'party',
-        'Number of Representants': 'total',
-        '% Votes with party': 'votesWithParty'
-    };
+  dataMembers.forEach( member => {
+    member.first_name = "<a href='" + member.url + "' target='_blank'>" + member.first_name + "</a>";
+  });
+  const tableData = getTableData(tableID, dataMembers)
+  if(tableID == 'Least'){
+    console.log(tableData)
+  }
 
+  let tableHead = "<tr>";
   for (let propertyName in tableProperties) {
       tableHead += "<th>" + propertyName + "</th>";
   }
@@ -111,15 +183,38 @@ function createGlanceTable(data) {
 
 let table = "";
 
-for (party in tableData){  
+tableData.forEach(row => {
   table+="<tr>";
   for(let property in tableProperties){
     property = tableProperties[property];
-    table += `<td>${tableData[party][property]}`;
+    table += `<td>${row[property]}`;
   }
   table+="</tr>";
-}
+
+});
 
 table = tableHead + table;
-document.getElementById("GlanceTable").innerHTML = table;
+document.getElementById(tableID).innerHTML = table;
+}
+
+
+
+// DATOS A LLAMAR PARA GENERAR LAS TABLAS 
+
+const glanceProperties = {
+            'Party': 'party',
+            'Number of Representants': 'total',
+            '% Votes with party': 'votesWithParty',
+        }
+
+const attendanceProperties = {
+  'Name': 'first_name',
+  'No. Missed Votes':'missed_votes',
+  '% Missed': 'missed_votes_pct'
+}
+
+const loyaltyProperties = {
+  'Name': 'first_name',
+  'No Party Votes':'total_votes',
+  '% Party votes': 'votes_with_party_pct'
 }
